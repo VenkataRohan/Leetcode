cc7afa4de592bb5e4cc8c5687fad9a3ecb324f4e
VenkataRohan <83659881+VenkataRohan@users.noreply.github.com>
2024-08-03 16:20:28 +0530

    Time: 7 ms (61.99%), Space: 59.9 MB (38.64%) - LeetHub

--- 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.java
@@ -25,11 +25,6 @@ class Solution {
         int[] r = solve(root.right);
 
         if(l[1] < root.val && r[2] > root.val){
-            if(root.val == 7){
-                System.out.println(root.val + " "+r[1]+ " "+r[2] + " "+ l[1]+" "+ l[2]);
-            }
-            // if(l[0]+r[0]+root.val < r[0] || l[0]+r[0]+root.val < l[0] )
-            // return new int[]{Math.max(r[0],l[0]),Integer.MAX_VALUE,Integer.MIN_VALUE};
             sum = Math.max(l[0]+r[0]+root.val,sum);
             return new int[]{l[0]+r[0]+root.val,Math.max(r[1],root.val),Math.min(l[2],root.val)};
 
